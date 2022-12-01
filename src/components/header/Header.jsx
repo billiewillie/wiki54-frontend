1ad4e1ce1c4276ee4fea8433a314ba930cce567b
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-12-01 17:00:07 +0300

    edited

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,15 @@ import styles from './Header.module.css';
 import Logo from '../logo/Logo';
 
 const Header = () => {
+	const logoutHandler = () => {};
+
 	return (
 		<header className={styles.header}>
 			<Logo />
 			<Search />
-			<div className='header-tools'>account info</div>
+			<div className='header-tools' onClick={logoutHandler}>
+				Log out
+			</div>
 		</header>
 	);
 };
--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,11 +1,18 @@
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
-
 const LoginPage = () => {
 	return (
-		<>
-			<div>LoginPage</div>
-			<NavLink to='/'>Home</NavLink>
-		</>
+		<div className='LoginPage'>
+			<h1>Данные для входа</h1>
+			<form action=''>
+				<div className='form-row'>
+					<label htmlFor='form-email'>email</label>
+					<input id='form-email' type='email' />
+				</div>
+				<div className='form-row'>
+					<label htmlFor='form-password'>пароль</label>
+					<input id='form-password' type='password' />
+				</div>
+			</form>
+		</div>
 	);
 };
 
